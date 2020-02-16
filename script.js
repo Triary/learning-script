31f4b9c8d64080bd0899f3a13ffff34575a96a55
Triary <dasha.kursanova@ya.ru>
2020-02-17 00:44:33 +0300

    second commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 "use strict";
 
-let money = prompt('Ваш бюджет на месяц?',' ');
+let money = +prompt('Ваш бюджет на месяц?',' ');
 let time  = prompt('Введите дату в формате YYYY-MM-DD',' ');
 
 let appData = {
@@ -14,11 +14,76 @@ let appData = {
 
 for(let i=0; i < 2; i++)
 {
-    let consumptionName = prompt('Введите обязательную статью расходов в этом месяце', ' ');
-    let consumptionNumber = prompt('Во сколько обойдется?', ' ');
-    appData.expenses.consumptionName = consumptionNumber;
+    let consumptionName = prompt('Введите обязательную статью расходов в этом месяце.', ' ');
+    let consumptionNumber = +prompt('Во сколько обойдется?', ' ');
+
+    if(typeof(consumptionName) === "string" && (typeof(consumptionName) != null && typeof(consumptionNumber) != null && consumptionNumber == ' ' 
+        && consumptionName == ' '  && consumptionName.length < 50) )
+        {
+            console.log("Done.");
+            appData.expenses.consumptionName = consumptionNumber;
+        }
+        else
+        {
+            console.log("Incorrect value.");
+            i--;
+        }
 }
 
+appData.moneyPerDay = appData.budget / 30;
+alert("бюджет на 1 день:  " + appData.moneyPerDay + "руб");
 
-alert(appData.budget / 30);
+
+if (appData.moneyPerDay <= 100) {
+    console.log ("Это минимальный уровень достатка.");
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log ("Это средний уровень достатка.");
+} else if (appData.moneyPerDay >= 2000) {
+    console.log ("Это высокий уровень достатка.");
+} else {
+    console.log ("ERROR");
+}
+ 
+
+// Цикл do-while
+
+// let i = 0;
+// do {
+//     let consumptionName = prompt ("Введите обязательную статью расходов в этом месяце", " "),
+//         consumptionNumber  = prompt ("Во сколько обойдется?", " ");
+
+//  if ( typeof(consumptionName ) === 'string' && typeof(consumptionName ) != null && typeof(consumptionNumber ) != null && consumptionName != ' ' 
+//       && consumptionNumber  != ' ' && consumptionName.length < 50) 
+//        {
+//          console.log ("Done.");
+//          appData.expenses.consumptionName  = consumptionNumber;
+//        } 
+//      else 
+//        {
+//          console.log ("Incorrect value.");
+//          i--;
+//        }
+// while(i < 2);
+
+
+
+// цикл while
+
+// let i = 0;
+// while (i < 2) {
+//    let consumptionName = prompt('Введите обязательную статью расходов в этом месяце.', ' ');
+//    let consumptionNumber = +prompt('Во сколько обойдется?', ' ');
+
+//     if ( typeof(consumptionName ) === 'string' && typeof(consumptionName ) != null && typeof(consumptionNumber ) != null && consumptionName != ' ' 
+//          && consumptionNumber  != ' ' && consumptionName.length < 50) {
+//         console.log ("Done.");
+//         appData.expenses.consumptionName  = consumptionNumber;
+//     } else {
+//          console.log ("Incorrect value.");
+//          i--;
+//     }
+
+//     i++;
+// }
+
 
